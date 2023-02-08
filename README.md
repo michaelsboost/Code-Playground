cb16fcf59276b9616dadd0e708c8072c930f9abc
Michael <mikethedj4@yahoo.com>
2023-02-07 19:21:03 -0600

    added lints and inline color picker

--- README.md
+++ README.md
@@ -1,11 +1,44 @@
-# Code-Playground--Save-Code-As-Gist
-This is a simple html/css/javascript playground that allows you to save and load your code via ajax as a github gist.
-
-You can try the demo out [here](http://michaelsboost.github.io/Code-Playground--Save-Code-As-Gist/#a0185028cdbb635ff17f05ca07b065ac):  
-http://michaelsboost.github.io/Code-Playground--Save-Code-As-Gist/#a0185028cdbb635ff17f05ca07b065ac
+# Code Playground
+This is a simple html/css/javascript code playground
 
 # I am no longer actively developing this app!
 
-You can show your appreciation by [Donating via SquareCash](https://cash.me/$michaelsboost) and/or [PayPal](https://www.paypal.me/mikethedj4)
+License
+-------------
 
-[![ko-fi](https://az743702.vo.msecnd.net/cdn/kofi2.png?v=0)](https://ko-fi.com/michaelsboost)
+MIT
+
+Tech
+-------------
+
+Code Playground uses a number of open source projects to work properly:
+
+* [jQuery](http://jquery.com/) - duh
+* [JQWidgets](http://www.jqwidgets.com/jquery-widgets-demo/demos/jqxsplitter/index.htm#demos/jqxsplitter/nested-splitters.htm) - awesome plugin for resizable columns
+* [Codemirror](http://codemirror.net/) - awesome web-based text editor
+* [Inlet](https://github.com/mikethedj4/Inlet) - awesome inline color picker and numeric slider for Codemirror
+* [Emmet](http://emmet.io/) - Codemirror Plugin for Zen Coding
+
+Development
+-------------
+
+Want to contribute? Great!  
+
+You can submit a pull request or simply share the project :)  
+
+Of course Code Playground is free and open source, so you can always fork the project and have fun :)  
+
+**As of February 7, 2023 Code Playground is no longer an active project.  
+All updates as of that date and on are solely contributor based implementations.**
+
+[![ko-fi](https://az743702.vo.msecnd.net/cdn/kofi2.png?v=0)](https://ko-fi.com/michaelsboost)  
+
+If Code Playground was at all helpful for you. You can show your appreciation a few ways...  
+
+1) Check out my Graphic Design Course: https://michaelsboost.com/graphicdesign  
+2) Registering on my store as a customer: https://michaelsboost.com/store  
+3) Buying me a coffee! http://ko-fi.com/michaelsboost  
+4) Purchasing one of my t-shirts: https://michaelsboost.com/gear  
+5) Purchasing any of my art: https://deviantart.com/michaelsboost/prints  
+6) Donating via PayPal: https://michaelsboost.com/donate  
+7) Donating via SquareCash: https://cash.me/$michaelsboost  
---
+++ js/app.js
@@ -0,0 +1,122 @@
+let delay
+
+HTMLHint.defaultRuleset = {
+  "tagname-lowercase": true,
+  "attr-lowercase": true,
+  "attr-value-double-quotes": true,
+  "doctype-first": false,
+  "tag-pair": true,
+  "spec-char-escape": true,
+  "id-unique": true,
+  "src-not-empty": true,
+  "attr-no-duplication": true
+}
+
+// Initialize CodeMirror editor
+const htmlEditor = CodeMirror.fromTextArea(document.getElementById("htmlEditor"), {
+  mode: "text/html",
+  tabMode: "indent",
+  styleActiveLine: true,
+  lineNumbers: true,
+  lineWrapping: true,
+  autoCloseTags: true,
+  foldGutter: true,
+  dragDrop : true,
+  lint: true,
+  gutters: ["CodeMirror-lint-markers", "CodeMirror-linenumbers", "CodeMirror-foldgutter"]
+})
+Inlet(htmlEditor)
+emmetCodeMirror(htmlEditor)
+
+// init css editor
+const cssEditor = CodeMirror.fromTextArea(document.getElementById("cssEditor"), {
+  mode: "text/css",
+  tabMode: "indent",
+  styleActiveLine: true,
+  lineNumbers: true,
+  lineWrapping: true,
+  autoCloseTags: true,
+  foldGutter: true,
+  dragDrop : true,
+  lint: true,
+  gutters: ["CodeMirror-lint-markers", "CodeMirror-linenumbers", "CodeMirror-foldgutter"]
+})
+Inlet(cssEditor)
+emmetCodeMirror(cssEditor)
+
+// init js editor
+const jsEditor = CodeMirror.fromTextArea(document.getElementById("jsEditor"), {
+  mode: "text/javascript",
+  tabMode: "indent",
+  styleActiveLine: true,
+  lineNumbers: true,
+  lineWrapping: true,
+  autoCloseTags: true,
+  foldGutter: true,
+  dragDrop : true,
+  lint: {
+    options: {
+      "asi": true
+    }
+  },
+  gutters: ["CodeMirror-lint-markers", "CodeMirror-linenumbers", "CodeMirror-foldgutter"]
+})
+Inlet(jsEditor)
+emmetCodeMirror(jsEditor)
+
+// Live preview
+htmlEditor.on("change", function() {
+  clearTimeout(delay)
+  delay = setTimeout(updatePreview, 300)
+})
+cssEditor.on("change", function() {
+  clearTimeout(delay)
+  delay = setTimeout(updatePreview, 300)
+})
+jsEditor.on("change", function() {
+  clearTimeout(delay)
+  delay = setTimeout(updatePreview, 300)
+})
+
+updatePreview = () => {
+  document.getElementById("previewElm").innerHTML = ""
+  var frame = document.createElement("iframe")
+  frame.setAttribute("id", "preview")
+  frame.setAttribute("sandbox", "allow-forms allow-modals allow-pointer-lock allow-popups allow-same-origin allow-scripts")
+  document.getElementById("previewElm").appendChild(frame)
+  var previewFrame = document.getElementById("preview")
+  var preview      =  previewFrame.contentDocument ||  previewFrame.contentWindow.document
+  preview.open()
+  preview.write(`<!DOCTYPE html>\n<html>\n  <head>\n    <title>a sample title</title>\n    <meta charset=\"utf-8\">\n    <meta name=\"viewport\" content=\"initial-scale=1.0\">\n    <meta name=\"description\" content=\"A sample description\">\n    <link rel="stylesheet" href="libraries/normalize/normalize.css" />\n    <style>\n${cssEditor.getValue()}\n    </style>\n  </head>\n  <body>\n${htmlEditor.getValue()}\n\n    <script>\n      ${jsEditor.getValue()}\n    </script>\n  </body>\n</html>`)
+  preview.close()
+}
+setTimeout(updatePreview, 300)
+
+// Splitter Grid
+$("#splitContainer, #leftSplitter, #rightSplitter").jqxSplitter({
+  theme: "metro"
+})
+$("#splitContainer").jqxSplitter({
+  height: "100%",
+  width: "100%",
+  orientation: "horizontal",
+  showSplitBar: true,
+  panels: [{ size: "50%",collapsible:false },
+           { size: "50%" }]
+})
+$("#leftSplitter").jqxSplitter({
+  height: "100%",
+  width: "100%",
+  orientation: "vertical",
+  showSplitBar: true,
+  panels: [{ size: "50%",collapsible:false },
+           { size: "50%"}]
+})
+$("#rightSplitter").jqxSplitter({
+  height: "100%",
+  width: "100%",
+  orientation: "vertical",
+  showSplitBar: true,
+  panels: [{ size: "50%"},
+           { size: "50%",collapsible:false }]
+})
